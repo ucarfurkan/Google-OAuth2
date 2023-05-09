0a961e1d3fcb38e8a25c7dc34bd32b6075295e73
ucarfurkan <furkanucar011@gmail.com>
2023-05-09 19:05:21 +0300

    init project

---
+++ src/main/java/com/ucarfurkan/security/GoogleOAuth2/GoogleOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.ucarfurkan.security.GoogleOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucarfurkan/security/GoogleOAuth2/GoogleOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucarfurkan.security.GoogleOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
